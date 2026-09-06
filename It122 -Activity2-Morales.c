76fff7b9fa3ce9fbb27115def212e801072b8f41
moralesaljeot-max <morales.aljeot@gmail.com>
2026-09-06 21:20:05 +0800

    It122 -Activity2-Morales.c

---
+++ It122 -Activity2-Morales.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("\n");
+
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("\n");
+
+
+    sum = num1 + num2;
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
